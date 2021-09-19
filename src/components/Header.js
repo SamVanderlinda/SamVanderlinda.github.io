115387d73895f4cd218dd2b75408d8f26a86393a
Bui <meganb90@uw.edu>
2021-09-19 00:33:58 -0700

    adding header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <h1 style={headingStyle}> Echo-Vids</h1>
+            <h2 style={subHeadingStyle}> A YouTube Video Recommender for the Sounds in Life</h2>
+        </>
     )
+}
+
+const headingStyle = {
+    color:'White',
+    backgroundColor:'#ff4931',
+    textAlign:"center"
+}
+
+const subHeadingStyle = {
+    color:'Grey',
+    textAlign:"center"
 }
 
 export default Header
